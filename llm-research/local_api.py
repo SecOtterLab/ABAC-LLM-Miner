145866c3750fdc9bb9c1521e2d35491070e3b743
SecOtterLab <secotterlab@gmail.com>
2025-09-26 02:17:33 -0700

    local api

--- llm-research/local_api.py
+++ llm-research/local_api.py
@@ -3,7 +3,8 @@ import requests
 import re
 from helper_functions import iterate_api_requests, prepend_text_to_file
 import sys
-
+from ollama import chat
+from ollama import ChatResponse
 #READ ME:
     # To run one must be hosting the right LLM on ollama 
     # our url is not avialble to users with an ip not in our white list
@@ -40,22 +41,36 @@ def local_api(gt_acl_file, gt_abac_rules_file, attribute_data_file, attribute_da
 def local_api_call(request_text):
     try:
         # model = "phi4-reasoning:14b"
-        model = "gpt-oss:120b"
+        # model = "gpt-oss:120b"
+        # model = "qwen3:0.6b"
         
-        # URL = "http://100.89.62.79:11434/api/generate"
-        URL = "http://localhost:11434"
+        # # URL = "http://100.89.62.79:11434/api/generate"
+        # URL = "http://localhost:11434"
 
-        payload = {
-            "model": model,
-            "prompt": request_text,
-            "stream": False  # still streams, but we capture
-            # "format" : "json" ollam does not respond with consistent json, too unpredicatble auto response is more predicatble with <think></think> tags
-        }
+        # payload = {
+        #     "model": model,
+        #     "prompt": request_text,
+        #     "stream": False  # still streams, but we capture
+        #     # "format" : "json" ollam does not respond with consistent json, too unpredicatble auto response is more predicatble with <think></think> tags
+        # }
 
-        r = requests.post(URL, json=payload, timeout=(10, 2000)) #10 = TCP connection 420 is timeout 
-        r.raise_for_status()
+        # r = requests.post(URL, json=payload, timeout=(10, 2000)) #10 = TCP connection 420 is timeout 
+        # r.raise_for_status()
 
-        response_message = r.json()["response"].strip()
+      
+
+        response: ChatResponse = chat(model='gemma3', messages=[
+        {
+            'role': 'user',
+            'content': 'Why is the sky blue?',
+        },
+        ])
+
+        # print(response['message']['content'])
+        # or access fields directly from the response object
+        print(response.message.content)
+
+        response_message = response['message']['content'].strip()
         final_output = re.sub(r"<think>.*?</think>\n?", "", response_message, flags=re.DOTALL)
         final_output = final_output.replace("\\", "")
         print(final_output)
@@ -63,12 +78,12 @@ def local_api_call(request_text):
     
     except Exception as e:
             prepend_text_to_file("llm-research/session/cache/statistics.cache",f"Error in local_api.local_api_call: {e}\n")
-            return
+            return      
 
 
 def main():
     request_text = "what is 5 to the 3rd power, answer only"
-    local_api_call(request_text)
+    print(local_api_call(request_text))
 
 if __name__ == "__main__":
     main()
